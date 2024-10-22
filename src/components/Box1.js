378e144b30c8297dd99bd57a79e609fc8a5fc872
HernanMolinaDev <hernanjmolinadev@gmail.com>
2024-10-22 08:50:22 -0100

    idiomas box1

--- src/components/Box1.js
+++ src/components/Box1.js
@@ -1,15 +1,24 @@
 import img1 from "../imgs/fotoperfil.png"
-
+import {useState} from 'react'; 
 const Box1=()=>{
+    const [showTextEnglish, setTextEnglish]=useState(true)
+    const changeText =()=>
+        setTextEnglish(!showTextEnglish);
+    
+
     return(
         <div className="div-box1 row">
             <div className="col-sm-12 col-md-6 col-xl-3">
         <img src={img1} alt="fotoperfil" className="img-box1"/>
         </div>
+        <button onClick={changeText}>EN</button>
         <div className="text-box1 col-sm-12 col-md-6 col-xl-9">
         <h1>¡BIENVENIDO!</h1>
         <h3>Quien Soy?</h3>
-        <p>¡Hola, soy Hernan Molina, un Frontend Developer apasionado por la creación de experiencias web cautivadoras. Mi enfoque combina diseño y desarrollo para dar vida a proyectos únicos y funcionales. Explora mi portafolio y descubre cómo transformo ideas en interfaces impactantes. ¡Gracias por visitar!</p>
+        <p>{ showTextEnglish
+        ?"¡Hola, soy Hernan Molina, un Frontend Developer apasionado por la creación de experiencias web cautivadoras. Mi enfoque combina diseño y desarrollo para dar vida a proyectos únicos y funcionales. Explora mi portafolio y descubre cómo transformo ideas en interfaces impactantes. ¡Gracias por visitar!"
+
+        :"Hello, I’m Hernan Molina, a Frontend Developer passionate about creating captivating web experiences. My approach combines design and development to bring unique and functional projects to life. Explore my portfolio and discover how I transform ideas into impactful interfaces. Thank you for visiting!"}</p>
         <button>DESCARGAR CV</button>   
         <button>CONTACTAR</button>
         </div>
--- src/components/Box2.js
+++ src/components/Box2.js
@@ -1,14 +1,23 @@
+import {useState} from 'react'; 
 import img1 from "../imgs/paginawebej.png"
+import imgFinanzasWebJS from "../imgs/vistaprevia-finanzasjs.png"
+
 const Box2=()=>{
+    const [showInfo, setShowInfo]=useState(false)
+    const mostrarInfo=()=>{
+        setShowInfo(!showInfo);
+
+    };
     return(
         <div className="div-box2 row">
         <h1>PORTFOLIO</h1>
 
     <div className="card-project col-sm-2 col-md-4 col-xl-2">
-        <img src={img1} alt="logo"/>
-        <h5>Nombre</h5>
-        <button>Visitar</button>
-        <button>info desarrollo</button>
+        <img src={imgFinanzasWebJS} alt="Vista previa pagina web"/>
+        <h5>Finanzas Web</h5>
+        <a id="img-vistaprevia" href="https://hernanmolinadev.github.io/PF-JS-CoderHouse-HernanMolina/" target="_blank"><button>Visitar</button></a>
+        <button onClick={mostrarInfo}>info desarrollo</button>
+        {showInfo && (<div className='info-project'><p>Para este proyecto se utilizo JS vainilla, Sass , boostrap y manejo de datos locales</p> </div>)}
         </div>
     <div className="card-project col-sm-12 col-md-4 col-xl-2">
         <img src={img1} alt="logo"/>
